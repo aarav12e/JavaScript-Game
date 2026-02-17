0547edee99522fbf4f5bf80eb4ed1fdf4b3c8b5e
aarav12e <aarav12f@gmail.com>
2026-02-17 22:37:32 +0530

    canva cloud added

--- Project4/script.js
+++ Project4/script.js
@@ -14,8 +14,8 @@ class Explosion {
         this.spriteHeight = 179;
         this.width = this.spriteWidth * 0.7;
         this.height = this.spriteHeight * 0.7;
-        this.x = x - this.width / 2;
-        this.y = y - this.height / 2;
+        this.x = x ;
+        this.y = y ;
         this.image = new Image();
         this.image.src = "./boom.png";
         this.frame = 0;
@@ -30,8 +30,11 @@ class Explosion {
 
     }
     draw() {
-        ctx.
-        ctx.drawImage(this.image, this.spriteWidth * this.frame, 0 , this.spriteWidth , this.spriteHeight , this.x , this.y , this.width , this.height);
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(this.angle);
+        ctx.drawImage(this.image, this.spriteWidth * this.frame, 0 , this.spriteWidth , this.spriteHeight , 0 - this.width/2 , 0 - this.height/2 , this.width , this.height);
+        ctx.restore();
     }
 }
 
