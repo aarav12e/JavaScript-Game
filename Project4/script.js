92bc01b03d5dc51560c3eeb23d817a4798731999
aarav12e <aarav12f@gmail.com>
2026-02-18 21:57:06 +0530

    project 4 2nd final complete and project 5 started

--- Project4/script.js
+++ Project4/script.js
@@ -21,8 +21,12 @@ class Explosion {
         this.frame = 0;
         this.timer = 0;
         this.angle = Math.random() * 6.2;
+        this.sound = new Audio();
+        this.sound.src = './Magi/Fire.wav';
+
     }
     update() {
+        if (this.frame === 0) this.sound.play();
         this.timer++;
         if (this.timer % 10 === 0) {
             this.frame++;
@@ -41,9 +45,9 @@ class Explosion {
 window.addEventListener("click", function(e) {
    createAnimation(e);
 });
-window.addEventListener("mousemove", function(e) {
-   createAnimation(e);
-});
+// window.addEventListener("mousemove", function(e) {
+//    createAnimation(e);
+// });
 
 function createAnimation(e) {
     let positionX = e.x - canvasPosition.left;
