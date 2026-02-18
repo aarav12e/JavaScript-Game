cb3de019076b293b1c6885f3fd2e8890e46f82f2
aarav12e <aarav12f@gmail.com>
2026-02-19 00:27:28 +0530

    pic added for project 5

--- Project5/script.js
+++ Project5/script.js
@@ -10,19 +10,28 @@ let lastTime = 0;
 let ravens = [];
 class Raven {
     constructor(){
-        this.width = 100;
-        this.height = 50;
+        this.spritWidth = 271;
+        this.spritHeight = 194;
+        this.sizeModifer 
+        this.width = this.spritWidth/2;
+        this.height = this.spritHeight/2;
         this.x = canvas.width;
         this.y = Math.random() * (canvas.height - this.height);
         this.directionX = Math.random() * 5 + 3;
-        this.directionY = Math.random() * 5 - 2.5;     
+        this.directionY = Math.random() * 5 - 2.5;  
+        this.markerForDeletion = false; 
+        this.image = new Image();
+        this.image.src = './raven.png';
+        
 
     }
     update(){
         this.x -= this.directionX;
+        if (this.x < 0 - this.width) this.markerForDeletion = true;
     }
     draw(){
         ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.image, sx , sy , sw , sh , this.x, this.y, this.width, this.height);
 
     }
 }
@@ -39,6 +48,7 @@ function animate(timestamp){
     };
     [...ravens].forEach(Object => Object.update());
     [...ravens].forEach(Object => Object.draw());
+    ravens = ravens.filter(Object => !Object.markerForDeletion);
 
     requestAnimationFrame(animate);
 
