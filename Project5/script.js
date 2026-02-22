461b62886d4d7f52b8e13ab6f0d0da23166e1ee9
aarav12e <aarav12f@gmail.com>
2026-02-22 21:46:25 +0530

    refactor Raven class to improve size calculation and animation frame handling

--- Project5/script.js
+++ Project5/script.js
@@ -12,9 +12,9 @@ class Raven {
     constructor(){
         this.spritWidth = 271;
         this.spritHeight = 194;
-        this.sizeModifer 
-        this.width = this.spritWidth/2;
-        this.height = this.spritHeight/2;
+        this.sizeModifer = Math.random() * 0.6 + 0.4;
+        this.width = this.spritWidth * this.sizeModifer;
+        this.height = this.spritHeight * this.sizeModifer;
         this.x = canvas.width;
         this.y = Math.random() * (canvas.height - this.height);
         this.directionX = Math.random() * 5 + 3;
@@ -22,16 +22,23 @@ class Raven {
         this.markerForDeletion = false; 
         this.image = new Image();
         this.image.src = './raven.png';
+        this.frame = 0;
+        this.maxFrame = 4;
+        this.timeSinceFlap = 0;
+        this.flapIntervel = 100;
         
 
     }
     update(){
         this.x -= this.directionX;
         if (this.x < 0 - this.width) this.markerForDeletion = true;
+        if (this.frame > this.maxFrame) this.frame = 0;
+        else this.frame++;
     }
     draw(){
-        ctx.fillRect(this.x, this.y, this.width, this.height);
-        ctx.drawImage(this.image, sx , sy , sw , sh , this.x, this.y, this.width, this.height);
+        //ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.strokeRect(this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.image, this.frame * this.spritWidth, 0 , this.spritWidth , this.spritHeight , this.x, this.y, this.width, this.height);
 
     }
 }
@@ -46,7 +53,7 @@ function animate(timestamp){
         ravens.push(new Raven());
         timeToNextRaven = 0;
     };
-    [...ravens].forEach(Object => Object.update());
+    [...ravens].forEach(Object => Object.update(deltatime));
     [...ravens].forEach(Object => Object.draw());
     ravens = ravens.filter(Object => !Object.markerForDeletion);
 
